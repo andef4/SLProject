84bfe52e2ecdb2b319642f53c080982fb0b7d68c
Fabio Anderegg <andef4@bfh.ch>
2018-05-26 11:28:01 +0200

    add snapchat exercise

--- exercises/cv13_Snapchat2D/cv13_Snapchat2D.cpp
+++ exercises/cv13_Snapchat2D/cv13_Snapchat2D.cpp
@@ -1,16 +1,17 @@
 //#############################################################################
-//  File:      cv07_MeshWarping.cpp
-//  Purpose:   Minimal OpenCV application that warps a triangular mesh
-//  Copyright: Based on Satya Mallick's Tutorials: https://www.learnopencv.com
-//  Date:      Spring 2018
+//  File:      cv13_Snapchat2D.cpp
+//  Purpose:   Minimal OpenCV app for a 2D Snapchatfilter
+//  Taken from Satya Mallic on: http://www.learnopencv.com
+//  Date:      Authumn 2017
 //#############################################################################
 
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <iostream>
-
-using namespace cv;
+#include <opencv2/opencv.hpp>
+#include <opencv2/face.hpp>
+ 
 using namespace std;
+using namespace cv;
+using namespace cv::face;
+
 
 //-----------------------------------------------------------------------------
 static void drawDelaunay(Mat& img, Subdiv2D& subdiv, Scalar delaunay_color)
@@ -27,7 +28,7 @@ static void drawDelaunay(Mat& img, Subdiv2D& subdiv, Scalar delaunay_color)
         pt[0] = Point(cvRound(t[0]), cvRound(t[1]));
         pt[1] = Point(cvRound(t[2]), cvRound(t[3]));
         pt[2] = Point(cvRound(t[4]), cvRound(t[5]));
-        
+
         // Draw rectangles completely inside the image.
         if (rect.contains(pt[0]) && rect.contains(pt[1]) && rect.contains(pt[2]))
         {   line(img, pt[0], pt[1], delaunay_color, 1, CV_AA, 0);
@@ -55,7 +56,7 @@ static void createDelaunay(Mat& img,
             waitKey(100);
         }
     }
-    
+
     // Unfortunately we don't get the triangles by there original point indexes.
     // We only get them with their vertex coordinates.
     // So we have to map them again to get the triangles with their point indexes.
@@ -65,14 +66,14 @@ static void createDelaunay(Mat& img,
     subdiv.getTriangleList(triangleList);
     vector<Point2f> pt(3);
     vector<int> ind(3);
-    
+
     for( size_t i = 0; i < triangleList.size(); i++ )
     {
         Vec6f t = triangleList[i];
         pt[0] = Point2f(t[0], t[1]);
         pt[1] = Point2f(t[2], t[3]);
         pt[2] = Point2f(t[4], t[5 ]);
-        
+
         if (rect.contains(pt[0]) &&
             rect.contains(pt[1]) &&
             rect.contains(pt[2]))
@@ -82,38 +83,12 @@ static void createDelaunay(Mat& img,
                     if(abs(pt[j].x - points[k].x) < 1.0 &&
                        abs(pt[j].y - points[k].y) < 1)
                         ind[j] = (int)k;
-            
+
             triangleIndexes.push_back(ind);
         }
     }
 }
-//-----------------------------------------------------------------------------
-static void drawVoronoi(Mat& img, Subdiv2D& subdiv)
-{
-    vector<vector<Point2f> > facets;
-    vector<Point2f> centers;
-    subdiv.getVoronoiFacetList(vector<int>(), facets, centers);
 
-    vector<Point> ifacet;
-    vector<vector<Point> > ifacets(1);
-
-    for(size_t i = 0; i < facets.size(); i++)
-    {
-        ifacet.resize(facets[i].size());
-        for(size_t j = 0; j < facets[i].size(); j++)
-            ifacet[j] = facets[i][j];
-
-        Scalar color;
-        color[0] = rand() & 255;
-        color[1] = rand() & 255;
-        color[2] = rand() & 255;
-        fillConvexPoly(img, ifacet, color, 8, 0);
-
-        ifacets[0] = ifacet;
-        polylines(img, ifacets, true, Scalar(), 1, CV_AA, 0);
-        circle(img, centers[i], 2, Scalar(), CV_FILLED, CV_AA, 0);
-    }
-}
 //-----------------------------------------------------------------------------
 // Warps a triangular regions from img1 to img2
 void warpTriangle(Mat &img1,
@@ -179,116 +154,108 @@ static void warpImage(Mat& img1,
         tri1.push_back(points1[triangles[i][0]]);
         tri1.push_back(points1[triangles[i][1]]);
         tri1.push_back(points1[triangles[i][2]]);
-        
+
         vector<Point2f> tri2;
         tri2.push_back(points2[triangles[i][0]]);
         tri2.push_back(points2[triangles[i][1]]);
         tri2.push_back(points2[triangles[i][2]]);
-        
+
         warpTriangle(img1, img2, tri1, tri2);
     }
 }
+
 //-----------------------------------------------------------------------------
 int main()
 {
-    // Read input image
+    // Load Face Detector
     // Note for Visual Studio: You must set the Working Directory to $(TargetDir)
-    // with: Right Click on Project > Properties > Debugging
-    Mat img_orig = imread("../_data/images/textures/donald_trump.jpg");
-    if (img_orig.empty())
-    {   cout << "Could not load image. Is the working dir correct?" << endl;
-        return -1;
-    }
-    
-    // Keep a copy around
-    Mat img1 = img_orig.clone();
-    
-    // Create a vector of 68 facial landmark points.
-    vector<Point2f> points = {
-        { 80,311},{ 80,357},{ 83,405},{ 88,454},
-        { 96,502},{114,546},{144,580},{180,607},
-        {226,616},{278,611},{335,591},{391,568},
-        {434,531},{464,486},{479,433},{487,377},
-        {494,321},{109,259},{126,238},{154,235},
-        {183,239},{212,248},{283,255},{321,244},
-        {359,240},{397,247},{427,271},{241,298},
-        {237,327},{232,354},{227,383},{201,418},
-        {215,423},{230,427},{250,425},{270,421},
-        {141,301},{159,293},{181,294},{199,309},
-        {178,313},{156,311},{309,312},{331,299},
-        {355,298},{376,309},{356,317},{331,317},
-        {177,503},{194,484},{213,473},{228,477},
-        {244,474},{271,488},{299,507},{271,523},
-        {244,528},{226,528},{209,525},{192,517},
-        {190,500},{213,489},{228,491},{244,491},
-        {286,504},{244,508},{228,507},{211,504}};
-
-    // Keep bounding rectangle around face points
-    Size size = img_orig.size();
-    Rect rectFace = boundingRect(points);
-    Point2f center(rectFace.x + rectFace.width/2,
-                   rectFace.y + rectFace.height/2);
-
-    // Add image border points
-    points.push_back(Point2d(0,0));
-    points.push_back(Point2d(size.width/2,0));
-    points.push_back(Point2d(size.width-1,0));
-    points.push_back(Point2d(size.width-1,size.height/2));
-    points.push_back(Point2d(size.width-1,size.height-1));
-    points.push_back(Point2d(size.width/2,size.height-1));
-    points.push_back(Point2d(0,size.height-1));
-    points.push_back(Point2d(0,size.height/2));
-    cout << points.size() << endl;
-    // Create an instance of Subdiv2D
-    Rect rect(0, 0, size.width, size.height);
-    Subdiv2D subdiv(rect);
-
-    // Create and draw the Delaunay triangulation
-    vector<vector<int>> triIndexes1;
-    createDelaunay(img1, subdiv, points, false, triIndexes1);
-    drawDelaunay(img1, subdiv, Scalar(255, 255, 255));
-
-    // Draw all points red
-    for (Point2f p : points)
-        circle(img1, p, 3, Scalar(0, 0, 255), CV_FILLED, CV_AA, 0);
-    
-    // Allocate space for voronoi Diagram
-    Mat img_voronoi = Mat::zeros(img1.rows, img1.cols, CV_8UC3);
-    
-    // Draw voronoi diagram
-    drawVoronoi(img_voronoi, subdiv);
-
-    // Show results.
-    imshow("Delaunay Triangulation", img1);
-    imshow("Voronoi Diagram", img_voronoi);
-    
-    // Do head warping with Donald Trumps face
-    // Copy the mesh points for warping
-    vector<Point2f> wPoints = points;
-    float scale = 1.0f;
-    float sign = 1.0;
-    img_orig.convertTo(img_orig, CV_32FC3, 1/255.0);
-    Mat imgW = Mat::ones(img_orig.size(), img_orig.type());
-    
-    // Loop scale between 0.8 and 1.2
-    while(true)
+    // with: Right Click on Project > Properties > Debugging 
+    CascadeClassifier faceDetector("../_data/opencv/haarcascades/haarcascade_frontalface_alt2.xml");
+ 
+    // Create an instance of Facemark
+    Ptr<Facemark> facemark = FacemarkLBF::create();
+ 
+    // Load landmark detector
+    facemark->loadModel("../_data/calibrations/lbfmodel.yaml");
+ 
+    // Set up webcam for video capture
+    VideoCapture cam(0);
+     
+    // Variable to store a video frame and its grayscale 
+    Mat frame, gray;
+     
+    // Read a frame
+    while(cam.read(frame))
     {
-        scale += sign * 0.01;
-        if (scale >= 1.2f || scale <= 0.8f) sign *= -1.0f;
+        // Convert frame to grayscale because faceDetector requires grayscale image
+        cvtColor(frame, gray, COLOR_BGR2GRAY);
+
+        // Detect faces
+        vector<Rect> faces;
+        int min = (int)(frame.rows*0.4f); // the bigger min the faster
+        int max = (int)(frame.rows*0.8f); // the smaller max the faster
+        cv::Size minSize(min, min);
+        cv::Size maxSize(max, max);
+        faceDetector.detectMultiScale(gray, faces, 1.1, 3, 0, minSize, maxSize);
+
+        // Variable for landmarks.
+        // Landmarks for one face is a vector of points
+        // There can be more than one face in the image. Hence, we
+        // use a vector of vector of points.
+        vector<vector<Point2f>> landmarks;
+
+        // Run landmark detector
+        bool success = facemark->fit(frame,faces,landmarks);
+
+        if(success && landmarks.size() >= 1)
+        {
+            // Draw rect of first face
+            rectangle(frame, faces[0], cv::Scalar(255, 0, 0), 2);
+
+
+            // Keep bounding rectangle around face points
+            Size size = frame.size();
+//            Rect rectFace = boundingRect(points);
+//            Point2f center(rectFace.x + rectFace.width/2,
+//                           rectFace.y + rectFace.height/2);
+
+//            // Add image border points
+            vector<Point2f> points;
+
+            for(int j=0; j < 68; j++) {
+                points.push_back(landmarks[0][j]);
+            }
+
+            points.push_back(Point2d(0,0));
+            points.push_back(Point2d(size.width/2,0));
+            points.push_back(Point2d(size.width-1,0));
+            points.push_back(Point2d(size.width-1,size.height/2));
+            points.push_back(Point2d(size.width-1,size.height-1));
+            points.push_back(Point2d(size.width/2,size.height-1));
+            points.push_back(Point2d(0,size.height-1));
+            points.push_back(Point2d(0,size.height/2));
+
+            // Create an instance of Subdiv2D
+            Rect rect(0, 0, size.width, size.height);
+            Subdiv2D subdiv(rect);
+
+            // Create and draw the Delaunay triangulation
+            vector<vector<int>> triIndexes1;
+            createDelaunay(frame, subdiv, points, false, triIndexes1);
+            drawDelaunay(frame, subdiv, Scalar(255, 255, 255));
+
+            // Draw landmarks of first face
+            //for(int j=0; j < 68; j++)
+            //    circle(frame, landmarks[0][j], 3, cv::Scalar(0, 0, 255), -1);
+        }
+
+        // Display results
+        imshow("Snapchat Filter", frame);
         
-        // Scale the face points from relative to the face cennter
-        for (int i=0; i<68; ++i)
-            wPoints[i] = ((points[i]-center) * scale) + center;
-
-        // Warp all triangles
-        warpImage(img_orig, imgW, points, wPoints, triIndexes1);
-        imshow("Warped Image", imgW);
-
         // Wait for key to exit loop
         if (waitKey(10) != -1)
             return 0;
     }
+    return 0;
 }
 //-----------------------------------------------------------------------------
-
-
